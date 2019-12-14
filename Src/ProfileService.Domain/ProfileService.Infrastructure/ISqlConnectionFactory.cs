e60e88b2f687c07831c18737d4a071db2ab37f46
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 13:41:35 +0100

    add Infrastructure layer

---
+++ Src/ProfileService.Domain/ProfileService.Infrastructure/ISqlConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace ProfileService.Infrastructure
+{
+    public interface ISqlConnectionFactory
+    {
+        IDbConnection GetOpenConnection();
+    }
+}
---
+++ Src/ProfileService.Domain/ProfileService.Infrastructure/SqlConnectionFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace ProfileService.Infrastructure
+{
+    public class SqlConnectionFactory : ISqlConnectionFactory, IDisposable
+    {
+        private readonly string _connectionString;
+        private IDbConnection _connection;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public IDbConnection GetOpenConnection()
+        {
+            if (this._connection == null || this._connection.State != ConnectionState.Open)
+            {
+                this._connection = new System.Data.SqlClient.SqlConnection(_connectionString);
+                this._connection.Open();
+            }
+
+            return this._connection;
+        }
+
+        public void Dispose()
+        {
+            if (this._connection != null && this._connection.State == ConnectionState.Open)
+            {
+                this._connection.Dispose();
+            }
+        }
+    }
+}
