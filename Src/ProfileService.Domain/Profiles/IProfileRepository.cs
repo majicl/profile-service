0de318f3e31fe44cc819568bcc384f28d3f34230
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 14:09:16 +0100

    replace rofileService.Infrastructure folders

---
+++ Src/ProfileService.Domain/Profiles/IProfileRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProfileService.Domain.Profiles
+{
+    public interface IProfileRepository
+    {
+        Task<UserProfile> GetCustomerProfileByIdAsync(Guid id);
+        Task<int> DeleteAsyncCustomerId(Guid id);
+        Task<int> CreateAsync(UserProfile userProfile);
+        Task<int> ModifyAsync(UserProfile userProfile);
+    }
+}
