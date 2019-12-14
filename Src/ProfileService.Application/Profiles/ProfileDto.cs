f35dc5f3ec723fdfd2f7135e1e7430f105ed35d4
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 19:07:58 +0100

    add GetProfileByCustomerId query to application

---
+++ Src/ProfileService.Application/Profiles/ProfileDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProfileService.Application.Profiles
+{
+    public class ProfileDto
+    {
+        public string SSN { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
---
+++ Src/ProfileService.Application/Profiles/Queries/GetProfileByCustomerId.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ProfileService.Application.Errors;
+using ProfileService.Domain.Profiles;
+
+namespace ProfileService.Application.Profiles.Queries
+{
+    public class GetProfileByCustonmerId
+    {
+        public class Query : IRequest<ProfileDto>
+        {
+            // should be provided with a Identity-Provider 
+            public Guid CustomerId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, ProfileDto>
+        {
+            private readonly IProfileRepository _profileRepository;
+            public Handler(IProfileRepository profileRepository)
+            {
+                _profileRepository = profileRepository;
+            }
+
+            public async Task<ProfileDto> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var profile = await _profileRepository.GetCustomerProfileByIdAsync(request.CustomerId, cancellationToken);
+
+                if (profile != null)
+                {
+                    return new ProfileDto
+                    {
+                        CountryCode = profile.MobilePhone.CountryCode,
+                        Email = profile.Email,
+                        PhoneNumber = profile.MobilePhone.Number,
+                        SSN = profile.SSN
+                    };
+                }
+
+                throw new RestException(System.Net.HttpStatusCode.InternalServerError, new { Message = "Problem finding related data" });
+            }
+        }
+    }
+}
