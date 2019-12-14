17a5394237b3f49161284c024dcdf23c93bc234a
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 19:21:15 +0100

    initiate profile controller with mediator

---
+++ Src/ProfileService.API/Controllers/ProfileController.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProfileService.Application.Profiles;
+using ProfileService.Application.Profiles.Commands;
+using ProfileService.Application.Profiles.Queries;
+
+namespace ProfileService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProfileController: ControllerBase
+    {
+        private IMediator _mediator { get; }
+
+        public ProfileController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Unit), (int)HttpStatusCode.OK)]
+        public async Task<Unit> Post([FromBody]CreateProfile.Command command)
+        {
+            var result = await _mediator.Send(command);
+            return result;
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Unit), (int)HttpStatusCode.OK)]
+        public async Task<Unit> Put([FromBody]ModifyProfile.Command command)
+        {
+            var result = await _mediator.Send(command);
+            return result;
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Unit), (int)HttpStatusCode.OK)]
+        public async Task<Unit> Delete([FromBody]DeleteProfile.Command command)
+        {
+            var result = await _mediator.Send(command);
+            return result;
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(ProfileDto), (int)HttpStatusCode.OK)]
+        public async Task<ProfileDto> Delete([FromBody]GetProfileByCustonmerId.Query query)
+        {
+            var result = await _mediator.Send(query);
+            return result;
+        }
+    }
+}
--- Src/ProfileService.API/Startup.cs
+++ Src/ProfileService.API/Startup.cs
@@ -40,8 +40,6 @@ namespace ProfileService.API
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
