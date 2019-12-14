36aa19489b725f3672d006797c957a7f0937a51a
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 18:55:33 +0100

    add deleteProfile command to the application

---
+++ Src/ProfileService.Application/Profiles/Commands/DeleteProfile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using ProfileService.Application.Errors;
+using ProfileService.Domain.Profiles;
+
+namespace ProfileService.Application.Profiles.Commands
+{
+    public class DeleteProfile
+    {
+        public class Command : IRequest
+        {
+            // should be provided with a Identity-Provider 
+            public Guid CustomerId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly IProfileRepository _profileRepository;
+            public Handler(IProfileRepository profileRepository)
+            {
+                _profileRepository = profileRepository;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var success = await _profileRepository.DeleteAsyncCustomerId(request.CustomerId, cancellationToken) > 0;
+
+                if (success) return Unit.Value;
+
+                throw new RestException(System.Net.HttpStatusCode.InternalServerError, new { Message = "Problem saving changes" });
+            }
+        }
+    }
+}
