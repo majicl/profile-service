efed70a7af8e748bee699e51e76b3c89a99ebaf0
Majid Parvin <majid.parvin@comeon.com>
2019-12-14 14:10:29 +0100

    add cancelation token to IProfileRepository

--- Src/ProfileService.Domain/Profiles/IProfileRepository.cs
+++ Src/ProfileService.Domain/Profiles/IProfileRepository.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProfileService.Domain.Profiles
 {
     public interface IProfileRepository
     {
-        Task<UserProfile> GetCustomerProfileByIdAsync(Guid id);
-        Task<int> DeleteAsyncCustomerId(Guid id);
-        Task<int> CreateAsync(UserProfile userProfile);
-        Task<int> ModifyAsync(UserProfile userProfile);
+        Task<UserProfile> GetCustomerProfileByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<int> DeleteAsyncCustomerId(Guid id, CancellationToken cancellationToken);
+        Task<int> CreateAsync(UserProfile userProfile, CancellationToken cancellationToken;
+        Task<int> ModifyAsync(UserProfile userProfile, CancellationToken cancellationToken);
     }
 }
